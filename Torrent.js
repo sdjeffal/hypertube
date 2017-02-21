69c73e9593604d12ee50730ea88d08e3cdb7078f
sdjeffal <sdjeffal@student.42.fr>
2017-02-21 20:22:20 +0100

    experimentation promise

--- Torrent.js
+++ Torrent.js
@@ -1,19 +1,20 @@
 'use strict'
 
-const imdb = require("imdb-api");
+const imdb = require('imdb-api');
 const yify = require('yify-search');
 const PirateBay = require('thepiratebay');
 const parser = require('torrent-name-parser');
+const lodash = require('lodash');
 
 export default class Torrent {
 
     constructor() {
-        this.torrent = undefined;
         this.site = undefined;
         this.title = undefined;
         this.magnet = undefined;
         this.cover = undefined;
         this.rating = undefined;
+        this.category = undefined;
         this.genre = undefined;
         this.year = undefined;
         this.summury = undefined;
@@ -23,20 +24,47 @@ export default class Torrent {
         this.actor = undefined;
         this.duration = undefined;
         this.quality = undefined;
+        this.resolution = undefined;
         this.seeder = undefined;
         this.peer = undefined;
         this.size = undefined;
-        this.type = undefined;
-        this.imdb = undefined;
-        this.info = undefined;
+        this.api = undefined;
     }
 
     search(name) {
-
         let promisePirate = new Promise((resolve, reject) => {
             PirateBay.search(name)
             .then(results => {
-                resolve(results)
+                if (!lodash.isEmpty(results)){
+                    let movies = [];
+                    lodash.forEach(results, (value) => {
+                        let movie = new Torrent
+                        let info = undefined
+                        info = parser(value.name)
+                        movie.title = info.title
+                        movie.quality = info.quality
+                        movie.resolution = info.resolution
+                        movie.language = info.language
+                        movie.category = value.subcategory.name
+                        movie.seeder = value.seeders
+                        movie.peer = value.leechers
+                        movie.magnet = value.magnetLink
+                        movie.size = value.size
+                        movie.api = value
+                        movie.site = 'thepiratebay';
+                        imdb.get(movie.title)
+                        .then(result => {
+                            movie.cover = result.poster
+                            movie.year = result.year
+                            movie.rating = result.rating
+                            movie.genre = result.genres
+                            movie.director = result.director
+                            movie.actor = result.actors
+                            movie.summary = result.plot
+                            movie.duration = result.runtime
+                        })
+                    })
+                }
             })
             .catch(err => {reject(err)})
         })
@@ -50,6 +78,27 @@ export default class Torrent {
             })
         })
 
-        return Promise.all([promisePirate, promiseYifi]).then(results => {return results}).catch(err => {return err})
+        return Promise.all([promisePirate, promiseYifi])
+        .then(results => {
+
+            if (!lodash.isEmpty(results[0]) && !lodash.isEmpty(results[1])){
+                console.log('torrent found on piratebay and yify');
+            }
+            else if (!lodash.isEmpty(results[0])){
+                console.log('torrent found only on piratebay');
+
+            }
+            else if (!lodash.isEmpty(results[1])){
+                console.log('torrent found only on yify');
+
+            }
+            else{
+                console.log('no torrent found');
+            }
+            return results
+        })
+        .catch(err => {
+            return err
+        })
     }
 }
--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import Torrent from './torrent'
 
 let t = new Torrent();
-t.search('naruto');
+t.search('avengers').then(results => {
+    //console.log(results)
+}).catch(err => {
+    //console.log(err);
+});
